c4ddb51a40a528148cc15b63d1a4c772a8bc1b00
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2023-09-05 10:56:54 +0300

    Grasp intials.

---
+++ JS FUNDAMENTALS/Fundamentals-one/arithmetics.js
@@ -0,0 +1,12 @@
+let x = 100 + 50;
+
+// Numbers are added strings are concatenated. 
+
+let y = 20;
+
+let z = x + y;
+
+let w = "srting"
+
+let v = z + w;
+console.log(v);
---
+++ JS FUNDAMENTALS/Fundamentals-one/task.js
@@ -0,0 +1,10 @@
+// Task one.
+let admin, name;
+
+name = 'John'
+admin = name
+alert(admin)
+
+// task two.
+let myPlanet = "Earth";
+let currentVisitor = "John Walter"
