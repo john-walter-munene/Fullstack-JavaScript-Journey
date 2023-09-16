97fd03491ef73f552ad802c65806163fd49cdeb1
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2023-09-16 04:40:05 +0300

    Good with functions.

---
+++ JS FUNDAMENTALS/Functions/arrowFunctions.js
@@ -0,0 +1,26 @@
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert( sum(1, 2) ); // 3
+
+let sayHi = () => alert("Hello!");
+
+sayHi();
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
+
---
+++ JS FUNDAMENTALS/Functions/callStack.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    return 1 + b;
+}
+
+function average(a, b) {
+    return add(a, b) / 2;
+}
+
+let x = average(10, 20);
