3e8a74811bee44f4571a903b250bfa44eec4a495
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2023-10-03 07:22:31 +0300

    Fine with DOM and Event Handling

---
+++ JS FUNDAMENTALS/DOM Manipulation Events/addEventListener/scrollPerformance.js
@@ -0,0 +1,38 @@
+const passive = document.querySelector("#passive");
+passive.addEventListener("change", (event) => {
+  container.removeEventListener("wheel", wheelHandler);
+  container.addEventListener("wheel", wheelHandler, {
+    passive: passive.checked,
+    once: true,
+  });
+});
+
+const container = document.querySelector("#container");
+container.addEventListener("wheel", wheelHandler, {
+  passive: true,
+  once: true,
+});
+
+function wheelHandler() {
+  function isPrime(n) {
+    for (let c = 2; c <= Math.sqrt(n); ++c) {
+      if (n % c === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  const quota = 1000000;
+  const primes = [];
+  const maximum = 1000000;
+
+  while (primes.length < quota) {
+    const candidate = Math.floor(Math.random() * (maximum + 1));
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+
+  console.log(primes);
+}
